962da3a841ee714eb5f4b86648150586011fdef3
Yuichiro_Seitoku <kazeso.kazewo@gmail.com>
2019-10-20 03:43:37 +0900

    マージができない事情があるのでファイル単位でmainブランチからとってきた

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Slack Rain</span>
+    <v-app-bar app class="purple darken-4">
+ 
+      <v-toolbar-title class="headline white--text center--text">
+
+        <span> Slack Rain</span>
       </v-toolbar-title>
-      <v-spacer></v-spacer>
+    
 
     </v-app-bar>
-      <transition name="page">
-        <router-view/>
-      </transition>
-    </v-app>
+
+        <transition name="page">
+            <router-view/>
+        </transition>
+
+  </v-app>
 </template>
 
 <script>
