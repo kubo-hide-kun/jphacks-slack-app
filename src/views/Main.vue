3b36f1a6f4a9790d34722d4e8ac5dd37a355be89
kubo-hide-kun <hide44041@gmail.com>
2019-10-20 04:17:42 +0900

    Improve code to be readable

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,63 +1,55 @@
 <template>
 <div id="app">
   <v-app id="inspire">
-
-<div class="p">
-
-<v-card id="create">
-
-
-      <v-speed-dial
-        v-model="fab"
-        :top="top"
-        :bottom="bottom"
-        :right="right"
-        :left="left"
-        :direction="direction"
-        :open-on-hover="hover"
-        :transition="transition"
-        
-      >
+    <div class="p">
+      <v-card id="create">
+        <v-speed-dial 
+            v-model="fab"
+          :top="top"
+          :bottom="bottom"
+          :right="right"
+          :left="left"
+          :direction="direction"
+          :open-on-hover="hover"
+          :transition="transition"
+        >
         <template v-slot:activator>
           <v-btn
             v-model="fab"
             color="blue darken-2"
             dark
             fab
-
-
-            
           >
-            <v-icon v-if="fab">mdi-close</v-icon>
-            <v-icon v-else>mdi-account-circle</v-icon>
-          </v-btn>
-        </template>
-        <v-btn
-          fab
-          dark
-          small
-          color="green"
-        >
-          <v-icon>mdi-pencil</v-icon>
+          <v-icon v-if="fab">mdi-close</v-icon>
+          <v-icon v-else>mdi-account-circle</v-icon>
         </v-btn>
-        <v-btn
-          fab
-          dark
-          small
-          color="indigo"
-        >
-          <v-icon>mdi-plus</v-icon>
-        </v-btn>
-        <v-btn
-          fab
-          dark
-          small
-          color="red"
-        >
-          <v-icon>mdi-delete</v-icon>
-        </v-btn>
-      </v-speed-dial>
-    </v-card>
+      </template>
+      <v-btn
+        fab
+        dark
+        small
+        color="green"
+      >
+        <v-icon>mdi-pencil</v-icon>
+      </v-btn>
+      <v-btn
+        fab
+        dark
+        small
+        color="indigo"
+      >
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
+      <v-btn
+        fab
+        dark
+        small
+        color="red"
+      >
+      <v-icon>mdi-delete</v-icon>
+      </v-btn>
+    </v-speed-dial>
+  </v-card>
 </div>
 
   </v-app>
